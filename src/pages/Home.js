dbb70f1757a014f5e040d8fbbedd4046873efc25
prasanna kundu <pkundu307@gmail.com>
2024-04-11 21:56:21 +0530

    homepage boilerplate added

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../features/navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+import Carousel from '../features/Carousel/Carousel'
+
+function Home() {
+  return (
+    <div>
+  
+<Navbar/>
+<Carousel/>
+<ProductList/>
+    </div>
+  )
+}
+
+export default Home
